8cc48d3805a6fa5588b3f017a20e3a4bf8617a32
Sonia Nigam <sonianigam@sbcglobal.net>
2017-02-22 14:24:20 -0600

    citation

--- script.py
+++ script.py
@@ -1,16 +1,68 @@
+###############################################################################
+# $Id$
+#
+# Project:	GDAL2Tiles, Google Summer of Code 2007 & 2008
+#           Global Map Tiles Classes
+# Purpose:	Convert a raster into TMS tiles, create KML SuperOverlay EPSG:4326,
+#			generate a simple HTML viewers based on Google Maps and OpenLayers
+# Author:	Klokan Petr Pridal, klokan at klokan dot cz
+# Web:		http://www.klokan.cz/projects/gdal2tiles/
+#
+###############################################################################
+# Copyright (c) 2008 Klokan Petr Pridal. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+"""
+globalmaptiles.py
+
+Global Map Tiles as defined in Tile Map Service (TMS) Profiles
+==============================================================
+
+Functions necessary for generation of global tiles used on the web.
+It contains classes implementing coordinate conversions for:
+
+  - GlobalMercator (based on EPSG:900913 = EPSG:3785)
+       for Google Maps, Yahoo Maps, Microsoft Maps compatible tiles
+  - GlobalGeodetic (based on EPSG:4326)
+       for OpenLayers Base Map and Google Earth compatible tiles
+
+More info at:
+
+http://wiki.osgeo.org/wiki/Tile_Map_Service_Specification
+http://wiki.osgeo.org/wiki/WMS_Tiling_Client_Recommendation
+http://msdn.microsoft.com/en-us/library/bb259689.aspx
+http://code.google.com/apis/maps/documentation/overlays.html#Google_Maps_Coordinates
+
+Created by Klokan Petr Pridal on 2008-07-03.
+Google Summer of Code 2008, project GDAL2Tiles for OSGEO.
+
+In case you use this class in your product, translate it to another language
+or find it usefull for your project please let me know.
+My email: klokan at klokan dot cz.
+I would like to know where it was used.
+
+Class is available under the open-source GDAL license (www.gdal.org).
+"""
+#####the following helper functions were taken from the open source project cited above  
 import math
-
-
-def centers(lat, lon, lat1, lon1):
-	"Compute the centers"
-	lat = float(lat)
-	lat = float(lon)
-	lat = float(lat1)
-	lat = float(lon1)
-	final_lat = (lat + lat1)/2.0
-	final_lon = (lon + lon1)/2.0
-
-	return final_lat, final_lon
 
 def LatLonToPixels(lat, lon, zoom):
 	"Converts lat/lon to pixel coordinates in given zoom of the EPSG:4326 pyramid"
@@ -42,11 +94,7 @@ def TileBounds(tx, ty, zoom):
 		(tx+1)*256*res - 180,
 		(ty+1)*256*res - 90
 	)
-def GoogleTile(tx, ty, zoom):
-		"Converts TMS tile coordinates to Google Tile coordinates"
-		
-		# coordinate origin is moved from bottom-left to top-left corner of the extent
-		return tx, (2**zoom - 1) - ty
+
 
 def QuadTree(tx, ty, zoom ):
 	"Converts TMS tile coordinates to Microsoft QuadTree"
@@ -62,3 +110,17 @@ def QuadTree(tx, ty, zoom ):
 			digit += 2
 		quadKey += str(digit)
 	return quadKey
+
+
+##### own code
+
+def centers(lat, lon, lat1, lon1):
+	"Compute the centers"
+	lat = float(lat)
+	lat = float(lon)
+	lat = float(lat1)
+	lat = float(lon1)
+	final_lat = (lat + lat1)/2.0
+	final_lon = (lon + lon1)/2.0
+
+	return final_lat, final_lon
